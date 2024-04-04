7c1eb0b16be3a4700e4a38d27f19585156a8e600
grbadas <grbadas@gmail.com>
2024-04-04 16:51:51 -0300

    primeiro commit

---
+++ src/main/java/badas/dev/gerenciamento/de/estoque/GerenciamentoDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package badas.dev.gerenciamento.de.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/badas/dev/gerenciamento/de/estoque/GerenciamentoDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package badas.dev.gerenciamento.de.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
