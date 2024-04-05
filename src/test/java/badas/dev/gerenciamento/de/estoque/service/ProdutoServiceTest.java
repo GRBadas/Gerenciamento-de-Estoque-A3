58d911092937e62f4dd38259afba020f7299678d
grbadas <grbadas@gmail.com>
2024-04-05 01:57:46 -0300

    test: Teste service de adicionar produto (red)

--- src/test/java/badas/dev/gerenciamento/de/estoque/service/ProdutoServiceTest.java
+++ src/test/java/badas/dev/gerenciamento/de/estoque/service/ProdutoServiceTest.java
@@ -1,4 +1,34 @@
 package badas.dev.gerenciamento.de.estoque.service;
 
+import badas.dev.gerenciamento.de.estoque.model.Produto;
+import badas.dev.gerenciamento.de.estoque.repository.ProdutoRepositorio;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 public class ProdutoServiceTest {
+
+    @Mock
+    private ProdutoRepositorio produtoRepositorio;
+
+    @InjectMocks
+    private ProdutoService produtoService;
+
+    @Test
+    public void testaddProduto() {
+
+        Produto produtoParaAdicionar = new Produto("Nome do produto", 10);
+        when(produtoRepositorio.save(produtoParaAdicionar)).thenReturn(produtoParaAdicionar);
+
+        Produto produtoAdicionado = produtoService.addProduto(produtoParaAdicionar);
+
+        assertNotNull(produtoAdicionado);
+        assertEquals("Product Name", produtoAdicionado.getName());
+        assertEquals(10, produtoAdicionado.getQuantity());
+        verify(produtoRepositorio, times(1)).save(produtoParaAdicionar);
+    }
 }
